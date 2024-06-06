c2b44726cfaee7146669a457279a35726e2a326e
Zain Ahmed <zainahmed01997@gmail.com>
2024-06-06 22:26:11 +0530

    Initial commit

---
+++ LL00.java
@@ -0,0 +1,10 @@
+public class LL00 {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList(1);
+        ll.append(2);
+        ll.append(4);
+        ll.prepend(0);
+        ll.insert(3,3);
+        ll.printList();
+    }
+}
